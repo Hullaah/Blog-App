7fce51bbeca91d378dbd98b84d2701493c98ea02
Hullaah <umaradelo1.247@gmail.com>
2024-08-10 17:32:10 +0100

    Added pagination for the post list

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
@@ -14,7 +15,7 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
@@ -29,8 +30,8 @@ class Post(models.Model):
         choices=Status,
         default=Status.DRAFT
     )
-    objects = models.Manager() # default manager
-    published = PublishedManager() # our custom manager
+    objects = models.Manager()  # default manager
+    published = PublishedManager()  # our custom manager
 
     class Meta:
         ordering = ['-publish']
@@ -40,3 +41,14 @@ class Post(models.Model):
 
     def __str__(self) -> str:
         return self.title
+
+    def get_absolute_url(self):
+        return reverse(
+            "blog:post_detail",
+            args=[
+                self.publish.year,
+                self.publish.month,
+                self.publish.day,
+                self.slug
+            ]
+        )
